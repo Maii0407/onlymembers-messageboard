533faa7ec70c21208b313aef8748746bb9e01887
Akmal Izuddin <izuddin0407@gmail.com>
2022-06-17 09:42:56 +0800

    Revert "minor changes: having problems with with validator: wrong arguments in body"
    This reverts commit 90d1773379951e2d3e8ba2560ec5a2e240527845.

--- controllers/postController.js
+++
@@ -1,5 +0,0 @@
-const Post = require( '../models/post' );
-
-exports.post_create_get = ( req, res, next ) => {};
-
-exports.post_create_post = ( req, res, next ) => {};
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,9 @@
-const express = require('express');
-const router = express.Router();
-
-const user_controller = require( '../controllers/userController' );
-const post_controller = require( '../controllers/postController' );
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
-router.get( '/', ( req, res, next ) => {
-  res.render( 'index', {
-    title: 'Express'
-  })
-})
-
-router.get( '/sign-up', user_controller.user_create_get);
-router.post( '/sign-up', user_controller.user_create_post);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
