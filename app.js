588a3b63fb401d3ffc92b7f610ea11534d946b0c
Akmal Izuddin <izuddin0407@gmail.com>
2022-06-22 10:58:58 +0800

    ready for deployment

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ passport.deserializeUser( ( id, done ) => {
 const app = express();
 
 const mongoose = require( 'mongoose' );
-const mongoDB = `${ process.env.MONGODB }`;
+const mongoDB = process.env.MONGODB_URI || `${ process.env.MONGODB }`;
 mongoose.connect( mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on( 'error', console.error.bind( console, 'MONGODB connection error' ));
--- controllers/postController.js
+++ controllers/postController.js
@@ -5,7 +5,7 @@ const { body, validationResult } = require( 'express-validator' );
 const async = require( 'async' );
 
 exports.postList = ( req, res, next ) => {
-  Post.find({}, 'title content date user').sort({ date: 1 }).populate( 'user' )
+  Post.find({}, 'title content date user').sort({ date: -1 }).populate( 'user' )
     .exec( ( err, list_posts ) => {
       if( err ) { return next( err ); }
 
@@ -25,7 +25,7 @@ exports.postCreateGet = ( req, res, next ) => {
 };
 
 exports.postCreatePost = [
-  body( 'title', 'Post title is required' ).trim().isLength({ min: 1 }).escape(),
+  body( 'title', 'Post title is required' ).trim().isLength({ min: 1 }).isLength({ max: 25 }).escape(),
   body( 'content', 'Post content is required' ).trim().isLength({ min: 1 }).escape(),
 
   ( req, res, next ) => {
